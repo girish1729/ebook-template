1b961af72f1dbd9779025c2eebb6ff2c7851ebf8
Girish Venkatachalam <girish@gayatri-hitech.com>
2022-10-12 07:57:20 +0530

    get house in order

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-pandoc -f markdown+smart --toc --epub-embed-font=fonts/*.ttf -o
-book.epub metadata.txt openssl-cheatsheet.md
+pandoc -f markdown+smart --toc -o book.epub metadata.txt source/sox-cheatsheet.md
 ebook-convert book.epub book.pdf
 rm book.epub
